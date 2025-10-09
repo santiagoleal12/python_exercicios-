50b9c91881eea870ef9d18dbbfda8b7d7beb141e
unknown <tabletchinocasanty@gmail.com>
2025-10-09 11:07:40 +0100

    primeiro ficheiro

---
+++ exe001.py
@@ -0,0 +1,6 @@
+nota1 = int(input("digite a primeira nota : "))
+nota2 = int(input("digite a segunda nota : "))
+
+soma = nota1 + nota2
+
+print ("a primeira é {}, segunda é {} soma das notas é {}" .format(nota1, nota2,soma))
