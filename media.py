51bd1782f3ddac855fa345d1a2ba7b3e58b3041f
unknown <tabletchinocasanty@gmail.com>
2025-10-14 17:10:28 +0100

    adicionei uma estrutura condicional

---
+++ media.py
@@ -0,0 +1,13 @@
+n1 = int(input("digte a primeira nota : "))
+n2 = int(input("digite a segunda nota : "))
+
+media = float((n1 + n2)/2)
+
+
+print (" A sua média é de : {}" .format(media))
+if media >= 10 :
+       print("Aluno aprovado")
+
+else: 
+       print("Aluno reprovado")
+       
